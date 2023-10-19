91cd8168bda1945be19329d66687b59d1571ae09
rz367 <rz367@cornell.edu>
2023-10-19 01:33:02 -0400

    random 3 asm fix

--- lab2_proc/asm/random3.asm
+++ lab2_proc/asm/random3.asm
@@ -1,43 +1,43 @@
-addi x1, x0, 3091
-addi x2, x0, 2887
-addi x3, x0, 1757
-addi x4, x0, 3503
-addi x5, x0, 1837
-addi x6, x0, 2551
-addi x7, x0, 2608
-addi x8, x0, 876
-addi x9, x0, 388
-addi x10, x0, 806
-addi x11, x0, 642
-addi x12, x0, 2334
-addi x13, x0, 624
-addi x14, x0, 1881
-addi x15, x0, 1998
-addi x16, x0, 3651
-addi x17, x0, 2017
-addi x18, x0, 2866
-addi x19, x0, 2923
-addi x20, x0, 3836
-xori x8, x1, 724
-slli x18, x2, 3
-slti x7, x15, 1271
-bne x13, x12, 8
-jal x17, 4
-addi x6, x5, 1289
-lui x7, 1046646
-sltu x18, x4, x4
-jal x7, 12
-sub x13, x2, x4
-bne x4, x10, 8
-sub x17, x2, x3
-blt x13, x11, 0
-slti x8, x9, 2530
-srai x14, x20, 29
-srli x8, x2, 21
-srl x5, x4, x15
-jal x12, 12
-bgeu x4, x16, 4
-sltiu x12, x6, 3129
+addi x1, x0, 2994
+addi x2, x0, 3764
+addi x3, x0, 3798
+addi x4, x0, 59
+addi x5, x0, 3923
+addi x6, x0, 3239
+addi x7, x0, 1464
+addi x8, x0, 1891
+addi x9, x0, 3997
+addi x10, x0, 1470
+addi x11, x0, 993
+addi x12, x0, 1375
+addi x13, x0, 517
+addi x14, x0, 2583
+addi x15, x0, 1406
+addi x16, x0, 3584
+addi x17, x0, 2790
+addi x18, x0, 1172
+addi x19, x0, 18
+addi x20, x0, 2789
+jal x9, 12
+beq x15, x12, 4
+or x1, x7, x15
+srai x19, x16, 2
+bltu x16, x15, 20
+ori x13, x1, 850
+srai x3, x14, 2
+mul x8, x18, x18
+srli x7, x16, 21
+blt x6, x7, 20
+jal x18, 0
+auipc x5, 820710
+jal x8, 0
+srl x11, x15, x4
+xor x5, x17, x15
+jal x15, 4
+and x4, x4, x13
+srai x12, x8, 11
+mul x11, x16, x17
+srli x6, x13, 19
 lui x1, 0x2000
 sw x2, 0(x1)
 sw x3, 4(x1)
