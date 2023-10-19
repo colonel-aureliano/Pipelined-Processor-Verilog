d069772fa6c7d30ecb8345ce06271d8ac193a803
jx353 <jx353@cornell.edu>
2023-10-19 09:58:44 -0400

    need to include utb, imm gen, alu tests for coverage as well

--- lab2_proc/run-coverage.sh
+++ lab2_proc/run-coverage.sh
@@ -18,5 +18,17 @@ for file in asm/*.asm; do
     make "$base_name.hex.sim" DESIGN="$DESIGN" RUN_ARG=--trace COVERAGE=coverage
 done
 
+# Data path ImmGen module coverage
+make utb_ProcDpathImmGen.v.sim DESIGN=ProcDpathImmGen COVERAGE=coverage
+
+# Data path ALU module coverage
+make utb_ProcDpathAlu.v.sim DESIGN=ProcDpathAlu COVERAGE=coverage
+
+# Unit test bench for the data path
+make "utb_${DESIGN}Dpath.v.sim" DESIGN="${DESIGN}DPath" COVERAGE=coverage
+
+# Unit test bench for the control unit
+make "utb_${DESIGN}Ctrl.v.sim" DESIGN="${DESIGN}Ctrl" COVERAGE=coverage
+
 # Make coverage report
 make coverage-report
